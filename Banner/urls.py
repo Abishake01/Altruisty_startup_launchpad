94f8129f8236f6c8f5a59ebdceaeb5ff5f09188b
Abishake01 <abishake381@gmail.com>
2025-02-27 23:02:01 +0530

    college home page image upload

--- Banner/urls.py
+++ Banner/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bannerform/',views.banner_uplod,name="bannerform"),
+    path('bannerform/',views.banner_upload,name="bannerform"),
     path('bannerlist/',views.banner_list,name="bannerlist"),
+    path('homepage_uplod/',views.homeiamge_upload,name="homeimage_upload"),
+    path('openimage_list/',views.homeimage_list,name="homeimage_list"),
     
 ]
--- Banner/views.py
+++ Banner/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Banner, HomeImage
 
 # Create your views here.
-def banner_uplod(request):
+def banner_upload(request):
     if request.method == 'POST':
         try:
             banner1 = request.FILES['banner1'].read() if 'banner1' in request.FILES else None
@@ -25,4 +25,26 @@ def banner_list(request):
     banners = Banner.objects.all()
     # Pass the data to the template
     return render(request, 'Master/instructor_list.html', {'banners': banners})
+
+def homeiamge_upload(request):
+    if request.method == 'POST':
+        try:
+            open_image = request.FILES['open_image'].read() if 'open_image' in request.FILES else None
+            
+            
+            banner = Banner(
+                open_image=open_image, 
+                
+                )
+            banner.save()
+        except Exception as e:
+            return HttpResponse(f"An error occurred: {e}")
+    return render(request, 'Master/openimage.html')
+
+def homeimage_list(request):
+    # Fetch all Instructor records from the database
+    open_images = HomeImage.objects.all()
+    # Pass the data to the template
+    return render(request, 'Master/openimage_list.html', {'open_images': open_images})
+
 
